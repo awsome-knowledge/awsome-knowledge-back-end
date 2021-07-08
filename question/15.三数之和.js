2fe95968d65ea4bd95a50d2d4b3c55876a535a49
qiufeihong2018 <qiufeihong2018@126.com>
2021-07-08 21:28:57 +0800

    update

--- question/15.三数之和.js
+++ question/15.三数之和.js
@@ -8,41 +8,15 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    arr = []
-    if (nums.length < 3) {
-        return arr
-    }
-    nums = nums.sort(function (a, b) {
-        return  a- b
-    })
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (nums[i] > 0) {
-            return arr
-        }
-        if (i > 0 && nums[i] == nums[i - 1]) {
-            continue
-        }
-
-        for (var j = i + 1, k = nums.length - 1; j < k;) {
-            if (nums[i] + nums[j] + nums[k] === 0) {
-                arr.push([nums[i], nums[j], nums[k]])
-                j++;
-                k--;
-                
-                while (j < k && nums[j] == nums[j - 1]) {
-                    j++
+    let arr = []
+    for (let i = 0; i < nums.length-2; i++) {
+        for (let j = i + 1; j < nums.length-1; j++) {
+            for (let x = j + 1; x < nums.length; x++) {
+                if ((nums[i] + nums[j] + nums[x])===0) {
+                    arr.push([nums[i], nums[j], nums[x]])
                 }
-                while (j < k && nums[k] == nums[k + 1]) {
-                    k--
-                }
-            } else if (nums[i] + nums[k] + nums[j] > 0) {
-                k--
-            } else {
-                j++
             }
-
         }
-
     }
     return arr
 };
