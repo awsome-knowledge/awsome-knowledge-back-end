969e3c65e2dcfa10e512bf71863a5d20ea817f87
qiufeihong2018 <qiufeihong2018@126.com>
2021-11-08 19:59:01 +0800

    update

---
+++ _book/question/822.翻转卡片游戏.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=822 lang=javascript
+ *
+ * [822] 翻转卡片游戏
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} fronts
+ * @param {number[]} backs
+ * @return {number}
+ */
+var flipgame = function(fronts, backs) {
+
+};
+// @lc code=end
+
