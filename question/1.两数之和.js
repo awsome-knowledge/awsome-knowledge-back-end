d491f7b36c92dfc3757c95729da8155c0ae68dd3
qiufeihong2018 <qiufeihong2018@126.com>
2022-02-21 21:08:59 +0800

    add

--- question/1.两数之和.js
+++ question/1.两数之和.js
@@ -9,13 +9,19 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+    // 声明一个字典
     let map = {}
+    // 循环遍历数组
     for (let i = 0; i < nums.length; i++) {
+        // 目标值与当前值做差
         let tep = target - nums[i]
+        // 差值是否在字典中
         let val = map.hasOwnProperty(tep)
+        // 如果找到，就返回他们的数组下标
         if (val) {
             return [map[tep], i]
         }
+        // 如果没找到，塞入字典中
         map[nums[i]] = i
     }
 };
